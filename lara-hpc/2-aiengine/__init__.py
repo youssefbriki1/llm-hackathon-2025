685d3a2d1f8c0d1dc283d1d0536e3d19262cc10c
giuseppefisicaro <62933389+giuseppefisicaro@users.noreply.github.com>
2025-09-09 13:15:22 +0200

    created main folders and magic chat
    Created the folder docs which will include all notebooks for developments and testing. Created the lara-hpc folder for the main code

---
+++ lara-hpc/2-aiengine/__init__.py
@@ -0,0 +1,9 @@
+def load_ipython_extension(ipython):
+    """
+    Any module file that define a function named `load_ipython_extension`
+    can be loaded via `%load_ext module.path` or be configured to be
+    autoloaded by IPython at startup time.
+    """
+    from magics import CompMatMagics
+
+    ipython.register_magics(CompMatMagics)
